ca2df14b24ad6eb0f34318d83cee5042da4622d0
Podsy Admin <admin@podsy.pro>
2026-07-25 03:19:51 +0700

    chore: remove all scratch work from github entirely

--- clear_db.ts
+++
@@ -1,6 +0,0 @@
-import { prisma } from './db.js';
-async function clearDB() {
-  await prisma.$executeRawUnsafe('TRUNCATE TABLE "Listing", "Shop", "ListingSnapshot", "ShopSnapshot", "TrackedItem" CASCADE;');
-  console.log('Database listings and shops cleared using CASCADE.');
-}
-clearDB().catch(console.error).finally(() => process.exit(0));
